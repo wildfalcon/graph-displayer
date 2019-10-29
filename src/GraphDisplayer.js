8a889ff3a5642d2dee84df41ebf05ac2e0584e04
Laurie Young <laurie@wildfalcon.com>
2019-10-29 14:47:07 +0000

    Wrap simple React component as a WebComponent

---
+++ src/GraphDisplayer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import pdfjsLib from "pdfjs-dist";
+
+// import './style.scss';
+// import { isEmptyObject, px, getImageDataAsDataUrl } from '../../../Utils';
+
+// pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
+
+const GraphDisplayer = props => {
+   
+
+    return (
+        <div className='graph-displayer'>
+            <p>I'm going to display a graph</p>
+        </div>
+    )
+}
+
+export default GraphDisplayer;
