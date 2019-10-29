73bcc8092df7963944c60e818bd3d71d31e57644
Laurie Young <laurie@wildfalcon.com>
2019-10-29 15:17:11 +0000

    Handle an event triggered within React component

--- src/GraphDisplayer.js
+++ src/GraphDisplayer.js
@@ -10,12 +10,19 @@ const GraphDisplayer = props => {
   const getConfig = () => {
     return JSON.parse(props.config);
   };
-   
+
+  const { raiseGraphClicked } = props;
+  const graphClicked = ev => {
+    raiseGraphClicked({
+      message: "Graph Clicked",
+    })
+  }
+
   const config = getConfig();
   const {name} = config; 
 
     return (
-        <div className='graph-displayer'>
+        <div className='graph-displayer' onClick={graphClicked}>
             <p>Hello {name}</p>
         </div>
     )
