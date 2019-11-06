c5186c7231f7bd06edefaba5d244106890394ce8
Laurie Young <laurie@wildfalcon.com>
2019-11-06 11:49:32 +0000

    Include GraphDisplayer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import {SimpleWebComponent} from "./main"
+import {GraphDisplayerWebComponent} from "./main"
 
 window.customElements.define('simple-web-component', SimpleWebComponent);  
+window.customElements.define('graph-displayer', GraphDisplayerWebComponent);  
