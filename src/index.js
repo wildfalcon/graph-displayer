18bf9c467f9e2e5bf959e2e33db34c224fb815e4
Laurie Young <laurie@wildfalcon.com>
2019-11-06 14:32:15 +0000

    Remove duplication between main and index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import {SimpleWebComponent} from "./main"
-import {GraphDisplayerWebComponent} from "./main"
+import {SimpleWebComponent} from './components/simple-web-component/simple-web-component.js'
+import {GraphDisplayerWebComponent} from './components/graph-displayer/graph-displayer.js'
+
 
 window.customElements.define('simple-web-component', SimpleWebComponent);  
 window.customElements.define('graph-displayer', GraphDisplayerWebComponent);  
--- src/main.js
+++
@@ -1,3 +0,0 @@
-export * from './components/simple-module/simple-module.js';
-export * from './components/simple-web-component/simple-web-component.js'
-export * from './components/graph-displayer/graph-displayer.js'
