15d000fb5ee68ec118d6f95e1a5b0d9bfef4042f
Laurie Young <laurie@wildfalcon.com>
2019-11-06 11:48:24 +0000

    Confiure Rollup to work with React

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import react from 'react';
+import reactDom from 'react-dom';
+import replace from '@rollup/plugin-replace';
 
 export default {
 	input: ['src/index.js'],
@@ -10,6 +14,18 @@ export default {
 	},
 	plugins: [
     resolve(),
-    babel()
+    babel(),
+    // This allows us to include React as a CommonJS module 
+    // And still have imports work
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        react: Object.keys(react),
+        'react-dom': Object.keys(reactDom)
+      }
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'production' )
+    })
   ]
 };
