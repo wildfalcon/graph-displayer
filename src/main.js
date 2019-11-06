b46066a73e26bd867c7f8ed8ceaacec9fe6ecc30
Laurie Young <laurie@wildfalcon.com>
2019-11-06 11:49:32 +0000

    Include GraphDisplayer

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 export * from './components/simple-module/simple-module.js';
 export * from './components/simple-web-component/simple-web-component.js'
+export * from './components/graph-displayer/graph-displayer.js'
